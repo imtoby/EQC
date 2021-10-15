16a02f39c5a482702d37b81a916ccfd7d5bc4737
ZhaoDongshuang <imtoby@126.com>
2021-10-15 21:36:56 +0800

    add new function

--- EQC/src/EStringUtils.cpp
+++ EQC/src/EStringUtils.cpp
@@ -7,3 +7,19 @@ QString EStringUtils::md5(const QByteArray &data) const
     QByteArray ba = QCryptographicHash::hash(data, QCryptographicHash::Md5);
     return ba.toHex();
 }
+
+QString EStringUtils::bytesToSizeUnitString(qint64 bytes) const
+{
+    const static QStringList SizeUnit = {"B", "KB", "MB", "GB", "TB"};
+
+    int nIndex = 0;
+    QString sStr = QString::number(bytes) + SizeUnit[0];
+    while (bytes > 1024 && nIndex < SizeUnit.size()) {
+        bytes /= 1024.0;
+        nIndex++;
+    }
+    if (nIndex > 0) {
+        sStr = QString::number(bytes).left(3) + SizeUnit[nIndex];
+    }
+    return sStr;
+}
--- EQC/src/EStringUtils.h
+++ EQC/src/EStringUtils.h
@@ -9,6 +9,9 @@ class EStringUtils : public QObject
     Q_OBJECT
 
     QString md5(const QByteArray& data) const;
+
+    // byte to "B" (for 0B), "KB", "MB", "GB", "TB"(May be ?)
+    QString bytesToSizeUnitString(qint64 bytes) const;
 };
 
 #endif // ESTRINGUTILS_H_79F37B59_2EE8_4F60_A7FA_6536B7AB03BF
