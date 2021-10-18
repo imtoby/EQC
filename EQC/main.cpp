320b3dbdfbec50c133fb2b140caf8cc9c38b19ab
ZhaoDongshuang <imtoby@126.com>
2021-10-18 21:36:53 +0800

    add test AES128_CBC_PKCS7
    Signed-off-by: ZhaoDongshuang <imtoby@126.com>

--- EQC/main.cpp
+++ EQC/main.cpp
@@ -1,8 +1,12 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QCryptographicHash>
+#include <QDebug>
 
 #include "src/EAsyncImageProvider.h"
 #include "SingleApplication-master/singleapplication.h"
+#include "qaesencryption.h"
+
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +15,29 @@ int main(int argc, char *argv[])
 #endif
 
     QGuiApplication app(argc, argv);
+
+    // test AES128_CBC_PKCS7 ======= start
+    QAESEncryption encryption(QAESEncryption::AES_128, QAESEncryption::CBC, QAESEncryption::PKCS7);
+    //16 byte string
+    QString inputStr("1234567890123456");
+    int blockLen = 16;
+    QString key("123456789123");
+
+    QByteArray iv;
+    quint8 iv_16[16]  = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+    for (int i=0; i<16; i++)
+        iv.append(iv_16[i]);
+    QByteArray hashKey = QCryptographicHash::hash(key.toLocal8Bit(), QCryptographicHash::Sha256);
+    QByteArray encodeText = encryption.encode(inputStr.toLocal8Bit(), hashKey, iv);
+    qDebug() << "encodeText:" << encodeText;
+    QByteArray decodeText = encryption.decode(encodeText, hashKey, iv);
+    qDebug() << "decodeText:" << decodeText;
+    QByteArray result = encryption.removePadding(decodeText);
+    qDebug() << "result:" << result;
+    QByteArray padding = decodeText.remove(0, encryption.removePadding(decodeText).length());
+    qDebug() << "padding:" << padding;
+    qDebug() << (padding.size() == blockLen);
+    // test AES128_CBC_PKCS7 ======= end
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
