3098d21856493bf0f475ac44ab35709d08216c7d
ZhaoDongshuang <imtoby@126.com>
2021-10-21 20:03:49 +0800

    mod: add new contents
    Signed-off-by: ZhaoDongshuang <imtoby@126.com>

--- EQC/src/EImageUtils.cpp
+++ EQC/src/EImageUtils.cpp
@@ -1,6 +1,33 @@
 #include "EImageUtils.h"
+#include <future>
+#include <QDebug>
+
+struct EImageUtilsPrivate
+{
+    std::future<void> m_fWorkThread;
+};
 
 EImageUtils::EImageUtils(QObject *parent) : QObject(parent)
 {
 
+}
+
+EImageUtils::~EImageUtils()
+{
+    if (d->m_fWorkThread.valid()) {
+        d->m_fWorkThread.get();
+    }
+}
+
+QImage EImageUtils::mergeImage(const QString &imagePath)
+{
+    if (d->m_fWorkThread.valid()) {
+        while (d->m_fWorkThread.wait_for(std::chrono::milliseconds(0))
+               != std::future_status::ready) {
+            qWarning() << "thread try connect is running";
+            return QImage();
+        }
+    }
+
+    return QImage();
 }
--- EQC/src/EImageUtils.h
+++ EQC/src/EImageUtils.h
@@ -3,15 +3,23 @@
 #define EIMAGEUTILS_H_D4BD9295_BF01_4835_84F7_DAC770A3C6DC
 
 #include <QObject>
+#include <QImage>
+
+struct EImageUtilsPrivate;
 
 class EImageUtils : public QObject
 {
     Q_OBJECT
 public:
     explicit EImageUtils(QObject *parent = nullptr);
+    ~EImageUtils();
+
+    QImage mergeImage(const QString& imagePath);
 
 signals:
 
+private:
+    EImageUtilsPrivate *d= nullptr;
 };
 
 #endif // EIMAGEUTILS_H_D4BD9295_BF01_4835_84F7_DAC770A3C6DC
