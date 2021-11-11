ddb1a873abc087c8a1a0039922e78f6eff12806c
ZhaoDongshuang <imtoby@126.com>
2021-11-11 21:16:21 +0800

    add new function

--- EQC/src/EClipboard.cpp
+++ EQC/src/EClipboard.cpp
@@ -1,6 +1,38 @@
 #include "EClipboard.h"
 
-EClipboard::EClipboard(QObject *parent) : QObject(parent)
-{
+#include <QApplication>
+#include <QClipboard>
+#include <QMimeData>
 
+
+struct HClipboardPrivate
+{
+    QClipboard *clipboard = nullptr;
+};
+
+EClipboard::EClipboard(QObject *parent)
+    : QObject(parent)
+    , d(new HClipboardPrivate)
+{
+    d->clipboard = qApp->clipboard();
+}
+
+EClipboard::~EClipboard()
+{
+    if (d) {
+        delete d;
+        d = nullptr;
+    }
+}
+
+void EClipboard::setHtml(const QString &html)
+{
+    QMimeData *mimeData = new QMimeData();
+    const QMimeData* oldMimeData = d->clipboard->mimeData();
+    const auto formats = oldMimeData->formats();
+    for (const auto& f : formats) {
+        mimeData->setData(f, oldMimeData->data(f));
+    }
+    mimeData->setHtml(html);
+    d->clipboard->setMimeData(mimeData);
 }
--- EQC/src/EClipboard.h
+++ EQC/src/EClipboard.h
@@ -4,14 +4,22 @@
 
 #include <QObject>
 
+struct HClipboardPrivate;
 class EClipboard : public QObject
 {
     Q_OBJECT
 public:
     explicit EClipboard(QObject *parent = nullptr);
+    ~EClipboard();
 
 signals:
+    void errorMessage(const QString& message);
 
+public slots:
+    void setHtml(const QString& html);
+
+private:
+    HClipboardPrivate *d = nullptr;
 };
 
 #endif // ECLIPBOARD_H_3863F9F2_F39E_4357_AEE7_BE54509B7FEE
