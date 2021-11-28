2b9d713dc3823748827d390371e0cf0b978611bc
ZhaoDongshuang <imtoby@126.com>
2021-11-28 10:33:25 +0800

    add use example

--- EQC/src/EReplyTimeout.h
+++ EQC/src/EReplyTimeout.h
@@ -19,3 +19,15 @@ private slots:
 };
 
 #endif // EREPLYTIMEOUT_H_854411AC_0795_4ABE_8888_ACA8862C8C84
+
+/*
+ * USE EXAMPLE
+ *
+QNetworkAccessManager *pManger = new QNetworkAccessManager(this);
+QNetworkReply *pReply = pManger->get(QNetworkRequest(QUrl("https://www.google.com")));
+EReplyTimeout *pTimeout = new EReplyTimeout(pReply, 1000);
+// 超时进一步处理
+connect(pTimeout, &EReplyTimeout::timeout, [=]() {
+    qDebug() << "Timeout";
+});
+ */
